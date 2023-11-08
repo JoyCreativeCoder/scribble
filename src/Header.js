8fec2c7d36adcedc197b12d904810911b267d0c9
joy <joy.nkwocha2003@gmail.com>
2023-11-08 12:04:22 +0100

    fixed new note functionality

--- src/Header.js
+++ src/Header.js
@@ -19,10 +19,11 @@ export default Header;
 export const HeaderTwo = ({ onAddNote }) => {
   return (
     <div className="header_two">
-      <span className="material-symbols-outlined" onClick={onAddNote}>
+      <span className="material-symbols-outlined" onClick={onAddNote} title="New Note">
         add
       </span>
-      <span className="material-symbols-outlined">more_horiz</span>
+      <span className="material-symbols-outlined" title="Save Note" >file_save</span>
+      <span className="material-symbols-outlined" title="Menu">more_horiz</span>
     </div>
   );
 };
